4ccf42af06ea29bcd9cfa7115e8c22c640a5852a
SCREECH115 <arekw99@gmail.com>
2023-01-29 14:04:38 +0100

    Added 0 when time > 10, added all days, hours, mins and seconds to new year

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ let hoursDocument = document.getElementById("hours");
 let minutesDocument = document.getElementById("minutes");
 let secondsDocument = document.getElementById("seconds");
 
+let daysToNewYear = document.getElementById("daysToNewYear");
+let hoursToNewYear = document.getElementById("hoursToNewYear");
+let minutesToNewYear = document.getElementById("minutesToNewYear");
+let secondsToNewYear = document.getElementById("secondsToNewYear");
+
 const timer = () => {
   const date = new Date();
   const newYearDate = new Date(newYear);
@@ -27,9 +32,23 @@ const timer = () => {
   console.log(daysLeft, hoursLeft, minutesLeft, secondsLeft);
 
   daysDocument.textContent = daysLeft;
-  hoursDocument.textContent = hoursLeft;
-  minutesDocument.textContent = minutesLeft;
-  secondsDocument.textContent = secondsLeft;
+  hoursDocument.textContent = formatZero(hoursLeft);
+  minutesDocument.textContent = formatZero(minutesLeft);
+  secondsDocument.textContent = formatZero(secondsLeft);
+
+  let daysAll = Math.floor(difference / 3600 / 24);
+  let hoursAll = Math.floor(difference / 60 / 60);
+  let minutesAll = Math.floor(difference / 60);
+  let secondsAll = Math.floor(difference);
+
+  daysToNewYear.textContent = daysAll;
+  hoursToNewYear.textContent = Math.floor(hoursAll);
+  minutesToNewYear.textContent = Math.floor(minutesAll);
+  secondsToNewYear.textContent = Math.floor(secondsAll);
+};
+
+const formatZero = (time) => {
+  return time < 10 ? `0${time}` : time;
 };
 
 setInterval(timer, 1000);
